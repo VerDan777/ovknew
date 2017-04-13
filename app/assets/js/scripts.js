3177a3f2a319130e08de99a8846d7ae779150ffa
Daniil <vereshhagin98@mail.ru>
2017-04-13 16:56:30 +0400

    add new sections and funny carousel)))

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -23,7 +23,7 @@ $(".main-head-toogle__menu").click(function() {
 		});
 	}
 },{
-    offset:'30%'
+    offset:'70%'
 });
 var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
 $(".lines").each(function() {
@@ -34,6 +34,16 @@ $(".lines").each(function() {
 		numberStep: comma_separator_number_step},
 		2000);
 });
+
+
+ $('.main-content__partners__carousel__partners').owlCarousel({
+	 loop:true,
+	 center:true,
+	 margin:10,
+	 stagePadding:20,	
+
+ });
+
 
 
 
